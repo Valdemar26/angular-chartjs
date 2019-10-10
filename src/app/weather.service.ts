e87138877653105ada0b2085af616894d53e876c
Volodymyr Zaplatynskii <v.zaplatunskuj@crosssoft.de>
2019-10-10 11:59:34 +0300

    change readme.md file

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -9,7 +9,9 @@ export class WeatherService {
   constructor(private http: HttpClient) {}
 
   dailyForecast() {
-    return this.http.get('http://api.openweathermap.org/data/2.5/forecast?id=524901&q=Lviv&APPID=1ec052023c49e1517c1f91424d764b7f&units=metric');
+    return this.http.get(
+      'http://api.openweathermap.org/data/2.5/forecast?id=524901&q=Lviv&APPID=1ec052023c49e1517c1f91424d764b7f&units=metric'
+    );
   }
 
 }
